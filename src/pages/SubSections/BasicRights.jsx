3a4b84c41f4d8d09ceff5d5136c3308e7a0e7ae6
Lateef Azeez <codelate18@gmail.com>
2020-08-27 21:27:56 -0600

    Added view pager with bugs

--- src/pages/SubSections/BasicRights.jsx
+++ src/pages/SubSections/BasicRights.jsx
@@ -1,11 +1,36 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, StyleSheet, Platform } from 'react-native';
+
+import SectionDetailsText from '../../components/SectionDetailsText';
+import colors from '../../config/colors';
+import AppViewPager from '../../components/AppViewPager';
 
 const BasicRights = () => {
   return (
     <View>
-      <Text>Basic Right Page</Text>
+      <View
+        style={{
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          backgroundColor: colors.white,
+          height: Platform.OS === 'android' ? 380 : 350,
+          padding: 30,
+        }}
+      >
+        <SectionDetailsText style={styles.sectionDetailsText}>
+          In Alberta, workers have 4 basic rights that relate to health safety:
+        </SectionDetailsText>
+        <AppViewPager />
+      </View>
     </View>
   );
 };
 export default BasicRights;
+
+const styles = StyleSheet.create({
+  sectionDetailsText: {
+    alignSelf: 'flex-start',
+    lineHeight: Platform.OS === 'android' ? 25 : 22,
+    fontSize: Platform.OS === 'android' ? 16 : 15,
+  },
+});
