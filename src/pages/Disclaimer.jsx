79f73609713aa3532b4c4476c950d0aeb4b2b727
Lateef Azeez <codelate18@gmail.com>
2020-09-03 03:25:49 -0600

    Added Resources and Dislaimer Pages

--- src/pages/Disclaimer.jsx
+++ src/pages/Disclaimer.jsx
@@ -1,10 +1,141 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, Image } from 'react-native';
+import { ScrollView } from 'react-native-gesture-handler';
+import { Paragraph, Subheading } from 'react-native-paper';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+
+import AppText from '../components/AppText';
+import colors from '../config/colors';
+import Bvc from '../../assets/bvc.png';
+import Alberta from '../../assets/alberta.png';
+import Annotation from '../../assets/annotation.png';
+import Sodexo from '../../assets/sodexo.png';
 
 const Disclaimer = () => (
-  <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-    <Text>Disclaimer Screen</Text>
-  </View>
+  <ScrollView style={styles.pageContainer}>
+    <View style={{ marginBottom: 30 }}>
+      <View style={styles.disclaimer}>
+        <MaterialCommunityIcons
+          name="information-outline"
+          size={35}
+          color="black"
+        />
+        <Text style={styles.heading}>Disclaimer</Text>
+      </View>
+      <Paragraph>
+        <AppText style={{ fontSize: 17 }}>
+          The purpose of this app is solely to educate workers about workplace
+          health and safety regulations, general advice, guidance on your
+          rights, and the services available to you. None of the information
+          provided is intended to be used for legal purposes.
+        </AppText>
+      </Paragraph>
+    </View>
+    <View style={styles.horizontalLine} />
+
+    <View style={styles.funders}>
+      <Text>Funded by the</Text>
+      <Subheading style={{ fontWeight: 'bold' }}>
+        Government of Alberta
+      </Subheading>
+      <Image
+        style={{
+          width: 150,
+          height: 75,
+          resizeMode: 'contain',
+          marginBottom: 15,
+        }}
+        source={Alberta}
+      />
+
+      <Subheading style={{ marginBottom: 15, fontWeight: 'bold' }}>
+        In partnership with:
+      </Subheading>
+      <View
+        style={{
+          backgroundColor: colors.black,
+          height: 60,
+          width: 260,
+          justifyContent: 'center',
+          alignItems: 'center',
+        }}
+      >
+        <Image style={{ width: 250, height: 40 }} source={Bvc} />
+      </View>
+      <View
+        style={{
+          flex: 1,
+          flexDirection: 'row',
+          justifyContent: 'center',
+
+          padding: 30,
+          marginBottom: 50,
+        }}
+      >
+        <View style={{ flex: 1 }}>
+          <Image
+            style={{
+              width: 120,
+              height: 75,
+              resizeMode: 'contain',
+              marginBottom: 15,
+            }}
+            source={Sodexo}
+          />
+        </View>
+        <View style={{ flex: 1, marginRight: 30 }}>
+          <Image
+            style={{
+              width: 180,
+              height: 150,
+              resizeMode: 'contain',
+              marginBottom: 15,
+            }}
+            source={Annotation}
+          />
+        </View>
+      </View>
+    </View>
+  </ScrollView>
 );
 
+const styles = StyleSheet.create({
+  alberta: {
+    flex: 1,
+    margin: 10,
+    width: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  disclaimer: {
+    flexDirection: 'row',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    marginLeft: 45,
+    marginBottom: 20,
+    marginTop: 20,
+  },
+  pageContainer: {
+    flex: 1,
+    padding: 20,
+    backgroundColor: colors.white,
+  },
+  heading: {
+    fontSize: 32,
+    fontWeight: 'bold',
+    marginLeft: 12,
+  },
+  horizontalLine: {
+    borderBottomColor: colors.lightGrey,
+    borderBottomWidth: 2,
+    width: '55%',
+    alignSelf: 'center',
+    marginBottom: 35,
+  },
+  funders: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+  },
+});
 export default Disclaimer;
--- src/pages/Resources.jsx
+++ src/pages/Resources.jsx
@@ -1,44 +1,175 @@
 import React from 'react';
-import { View, StyleSheet, Image } from 'react-native';
-import { ScrollView } from 'react-native-gesture-handler';
+import {
+  View,
+  StyleSheet,
+  Image,
+  Linking,
+  Text,
+  SafeAreaView,
+} from 'react-native';
+import { ScrollView, RectButton } from 'react-native-gesture-handler';
 import { Paragraph, Subheading } from 'react-native-paper';
+import { FontAwesome, SimpleLineIcons } from '@expo/vector-icons';
+import call from 'react-native-phone-call';
 
 import ResourcesImage from '../../assets/resources_icon.png';
 import AppText from '../components/AppText';
 
+import colors from '../config/colors';
+
+const CallAlberta = {
+  number: '4036904031',
+  prompt: true,
+};
+
+const CallCalgary = {
+  number: '4036904031',
+  prompt: true,
+};
+
 const Resources = () => (
-  <ScrollView style={styles.pageContainer}>
-    <View style={styles.pageImageContainer}>
-      <Image style={styles.pageImage} source={ResourcesImage} />
-    </View>
-    <View style={styles.pageTextContainer}>
-      <Paragraph>
-        <AppText>
-          The Alberta Worker’s Health Centre and Calgary Workers Resource Centre
-          provide free help for people who have issues with health and safety
-          and worker rights:
-        </AppText>
-      </Paragraph>
-      <Subheading
-        style={{ marginTop: 30, marginBottom: 15, fontWeight: 'bold' }}
-      >
-        Alberta Workers Health Centre
-      </Subheading>
-      <Paragraph>
-        <AppText>
-          Services are free. They provide a first point of contact for workers
-          with occupational health and safety issues in their workplace. They
-          will provide support, public legal education, and referrals.
-        </AppText>
-      </Paragraph>
-    </View>
-  </ScrollView>
+  <SafeAreaView style={styles.pageContainer}>
+    <ScrollView style={{ padding: 25 }}>
+      <View style={styles.pageImageContainer}>
+        <Image style={styles.pageImage} source={ResourcesImage} />
+      </View>
+      <View style={styles.pageTextContainer}>
+        <Paragraph>
+          <AppText>
+            The Alberta Worker’s Health Centre and Calgary Workers Resource
+            Centre provide free help for people who have issues with health and
+            safety and worker rights:
+          </AppText>
+        </Paragraph>
+        <Subheading
+          style={{ marginTop: 30, marginBottom: 15, fontWeight: 'bold' }}
+        >
+          Alberta Workers Health Centre
+        </Subheading>
+        <Paragraph style={{ marginBottom: 25 }}>
+          <AppText>
+            Services are free. They provide a first point of contact for workers
+            with occupational health and safety issues in their workplace. They
+            will provide support, public legal education, and referrals.
+          </AppText>
+        </Paragraph>
+        <View
+          style={{
+            shadowColor: colors.black,
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+            elevation: 50,
+          }}
+        >
+          <RectButton
+            onPress={() => call(CallAlberta).catch(console.error)}
+            style={styles.button}
+          >
+            <View style={styles.buttonContent} accessible>
+              <FontAwesome name="phone" size={24} color="black" />
+              <Text style={{ marginStart: 20 }}>780-486-9009</Text>
+            </View>
+          </RectButton>
+        </View>
+        <View
+          style={{
+            shadowColor: colors.black,
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+            elevation: 50,
+          }}
+        >
+          <RectButton
+            onPress={() => {
+              Linking.openURL('https://workershealthcentre.ca/');
+            }}
+            style={styles.button}
+          >
+            <View style={styles.buttonContent} accessible>
+              <SimpleLineIcons name="globe" size={24} color="black" />
+              <Text style={{ marginStart: 20 }}>
+                http://wwww.workershealthcentre.ca/
+              </Text>
+            </View>
+          </RectButton>
+        </View>
+        <Subheading
+          style={{ marginTop: 30, marginBottom: 15, fontWeight: 'bold' }}
+        >
+          Calgary Workers Resource Centre
+        </Subheading>
+        <Paragraph style={{ marginBottom: 25 }}>
+          <AppText>
+            They provide free case work and a public legal education program if
+            you are having issues understanding your rights in your workplace.
+          </AppText>
+        </Paragraph>
+        <View
+          style={{
+            shadowColor: colors.black,
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+            elevation: 50,
+          }}
+        >
+          <RectButton
+            onPress={() => call(CallCalgary).catch(console.error)}
+            style={styles.button}
+          >
+            <View style={styles.buttonContent} accessible>
+              <FontAwesome name="phone" size={24} color="black" />
+              <Text style={{ marginStart: 20 }}>403-264-8100</Text>
+            </View>
+          </RectButton>
+        </View>
+        <View
+          style={{
+            shadowColor: colors.black,
+            shadowOffset: { width: 0, height: 0 },
+            shadowOpacity: 0.3,
+            shadowRadius: 5,
+            elevation: 50,
+          }}
+        >
+          <RectButton
+            onPress={() => {
+              Linking.openURL('https://workershealthcentre.ca/');
+            }}
+            style={styles.button}
+          >
+            <View style={styles.buttonContent} accessible>
+              <SimpleLineIcons name="globe" size={24} color="black" />
+              <Text style={{ marginStart: 20 }}>
+                http://wwww.workershealthcentre.ca/
+              </Text>
+            </View>
+          </RectButton>
+        </View>
+      </View>
+    </ScrollView>
+  </SafeAreaView>
 );
 
 const styles = StyleSheet.create({
+  button: {
+    justifyContent: 'center',
+
+    height: 45,
+    borderRadius: 10,
+    backgroundColor: colors.white,
+    paddingLeft: 20,
+    marginBottom: 20,
+  },
+  buttonContent: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
   pageContainer: {
     flex: 1,
-    padding: 25,
   },
   pageImageContainer: {
     width: '100%',
@@ -48,7 +179,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   pageTextContainer: {
-    flex: 3,
+    flex: 1,
+    marginBottom: 50,
   },
   pageImage: {
     width: 130,
