7f9cfb4076186aae0550e5bf2456c365b62cd351
Bruno Mendonca <me@brunomendonca.com>
2020-09-15 13:27:33 -0600

    Add glossary items sorting

--- src/pages/Glossary.jsx
+++ src/pages/Glossary.jsx
@@ -6,27 +6,34 @@ import {
   SafeAreaView,
   SectionList,
 } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
+import FloatingButton from '../components/FloatingButton';
 import colors from '../config/colors';
 
-const DATA = [
+const listData = [
   {
     title: 'A',
-    data: ['List', 'Is', 'Empty'],
+    data: ['List', 'Is', 'Empty', 'A', 'Ab', 'Abc', 'Abcd'],
   },
   {
     title: 'B',
-    data: ['List', 'Is', 'Empty'],
+    data: ['List', 'Is', 'Empty', 'B', 'Ba', 'Bac', 'Bacd'],
   },
   {
     title: 'C',
-    data: ['List', 'Is', 'Empty'],
+    data: ['List', 'Is', 'Empty', 'C', 'Ca', 'Cab', 'Cabd'],
   },
   {
     title: 'D',
-    data: ['List', 'Is', 'Empty'],
+    data: ['List', 'Is', 'Empty', 'D', 'Da', 'Dab', 'Dabc'],
   },
 ];
+
+const sortedData = listData.map(item => ({
+  title: item.title,
+  data: item.data.sort(),
+}));
 
 const Item = ({ title }) => (
   <View style={styles.item}>
@@ -34,18 +41,25 @@ const Item = ({ title }) => (
   </View>
 );
 
-const Glossary = () => (
-  <SafeAreaView style={styles.container}>
-    <SectionList
-      sections={DATA}
-      keyExtractor={(item, index) => item + index}
-      renderItem={({ item }) => <Item title={item} />}
-      renderSectionHeader={({ section: { title } }) => (
-        <Text style={styles.header}>{title}</Text>
-      )}
-    />
-  </SafeAreaView>
-);
+const Glossary = () => {
+  const navigation = useNavigation();
+
+  return (
+    <>
+      <View style={styles.container}>
+        <SectionList
+          sections={sortedData}
+          keyExtractor={(item, index) => item + index}
+          renderItem={({ item }) => <Item title={item} />}
+          renderSectionHeader={({ section: { title } }) => (
+            <Text style={styles.header}>{title}</Text>
+          )}
+        />
+      </View>
+      <FloatingButton onPress={() => navigation.navigate('Find Your Voice')} />
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   container: {
