875543f83d6e079354230fe118e6538d874b7664
Bruno Mendonca <me@brunomendonca.com>
2020-09-22 13:15:48 -0600

    Change Chatbot colors

--- src/components/ChatBot.jsx
+++ src/components/ChatBot.jsx
@@ -26,11 +26,9 @@ const FyvChatBot = ({ steps, handleEnd, ...props }) => {
         borderWidth: 0,
         marginLeft: 43,
       }}
-      bubbleStyle={{ backgroundColor: colors.white }}
+      bubbleStyle={{ backgroundColor: colors.primary }}
       optionElementStyle={{
-        backgroundColor: colors.white,
-        borderWidth: 1,
-        borderColor: colors.primary,
+        backgroundColor: colors.fabButton,
         padding: 20,
         marginLeft: 50,
 
