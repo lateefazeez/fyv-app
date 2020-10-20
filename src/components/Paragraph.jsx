6dd8c51bef56fa10b0e9540692672e9e97e38f7e
Bruno Mendonca <me@brunomendonca.com>
2020-10-20 14:52:49 -0600

    Change glossary tooltip

--- src/components/Paragraph.jsx
+++ src/components/Paragraph.jsx
@@ -1,34 +1,135 @@
-import React from 'react';
-import { Text, Platform, StyleSheet } from 'react-native';
+import React, { useState } from 'react';
+import {
+  Text,
+  View,
+  Platform,
+  StyleSheet,
+  Modal,
+  TouchableOpacity,
+} from 'react-native';
 import ParsedText from 'react-native-parsed-text';
-import { testAlert } from 'utils';
 
+import Glossary from 'config/glossary.json';
 import colors from 'config/colors.json';
 
 const Paragraph = ({ children, style }) => {
-  const renderGlossary = (matchingString, matches) => {
+  const [foundWord, setFoundWord] = useState();
+
+  const [tooltipVisible, setTooltipVisible] = useState(false);
+  const [tooltipPosition, setTooltipPosition] = useState({ x: 0, y: 0 });
+
+  const renderGlossary = matchingString => {
     const pattern = /\[(.*?)\]/i;
     const match = matchingString.match(pattern);
     return `${match[1]}`;
   };
+
+  const displayTooltip = event => {
+    const currentWord = event.replace(/[[\]']+/g, '');
+    console.log('currentWord: ', currentWord);
+
+    Glossary.forEach(item => {
+      if (item.word.toUpperCase() === currentWord.toUpperCase()) {
+        console.log('foundWord: ', item);
+        setFoundWord(() => item);
+      }
+    });
+
+    if (foundWord) setTooltipVisible(true);
+  };
+
+  const hideTooltip = () => {
+    setFoundWord();
+    setTooltipVisible(false);
+  };
+
   return (
-    <ParsedText
-      style={[styles.text, style]}
-      parse={[
-        {
-          pattern: /\[(.*?)\]/i,
-          style: { fontWeight: 'bold', color: colors.primary },
-          onPress: testAlert,
-          renderText: renderGlossary,
-        },
-      ]}
-    >
-      {children}
-    </ParsedText>
+    <>
+      <View style={styles.centeredView}>
+        <Modal
+          transparent
+          animationType="fade"
+          visible={tooltipVisible}
+          onRequestClose={hideTooltip}
+        >
+          <TouchableOpacity
+            style={styles.tooltipContainer}
+            onPress={hideTooltip}
+          >
+            <TouchableOpacity
+              style={[
+                styles.tooltip,
+                {
+                  position: 'absolute',
+                  left: tooltipPosition.x,
+                  top: tooltipPosition.y - 72,
+                },
+              ]}
+              onPress={hideTooltip}
+            >
+              {foundWord && (
+                <>
+                  <Text style={styles.tooltipText}>
+                    {`Word: ${foundWord.word}`}
+                  </Text>
+                  <Text style={styles.tooltipText}>
+                    {`Phonetics: ${foundWord.phonetics}`}
+                  </Text>
+                  <Text style={styles.tooltipText}>
+                    {`Category: ${foundWord.category}`}
+                  </Text>
+                  <Text style={styles.tooltipText}>
+                    {`Description: ${foundWord.description}`}
+                  </Text>
+                </>
+              )}
+            </TouchableOpacity>
+          </TouchableOpacity>
+        </Modal>
+      </View>
+      <View
+        onTouchStart={event => {
+          setTooltipPosition({
+            x: event.nativeEvent.pageX,
+            y: event.nativeEvent.pageY + 56,
+          });
+        }}
+      >
+        <ParsedText
+          style={[styles.text, style]}
+          parse={[
+            {
+              pattern: /\[(.*?)\]/i,
+              style: styles.glossaryText,
+              onPress: displayTooltip,
+              renderText: renderGlossary,
+            },
+          ]}
+        >
+          {children}
+        </ParsedText>
+      </View>
+    </>
   );
 };
 
 const styles = StyleSheet.create({
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  tooltipContainer: {
+    flex: 1,
+    backgroundColor: colors.transpWhite,
+    position: 'relative',
+  },
+  tooltip: {
+    backgroundColor: colors.primary,
+    borderRadius: 8,
+    padding: 16,
+    maxWidth: 240,
+  },
   text: {
     color: colors.darkerGrey,
     lineHeight: 24,
@@ -36,6 +137,16 @@ const styles = StyleSheet.create({
     fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
     marginBottom: 24,
     textAlign: 'justify',
+  },
+  glossaryText: {
+    fontWeight: 'bold',
+    color: colors.primary,
+  },
+  tooltipText: {
+    color: colors.white,
+    lineHeight: 24,
+    fontSize: 16,
+    fontFamily: Platform.OS === 'android' ? 'Roboto' : 'Avenir',
   },
 });
 
