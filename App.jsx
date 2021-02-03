b0ba7c1a22367c1352ed136bf535218beb7c2578
Lateef Azeez <codelate18@gmail.com>
2021-02-03 15:27:03 -0700

    Resolved blank screen loading

--- App.jsx
+++ App.jsx
@@ -3,14 +3,39 @@ import React, { useState, useEffect } from 'react';
 import DrawerNavigation from 'navigation/DrawerNavigation';
 import IntroSlider from 'screens/IntroSlider';
 import fetchAndStoreContent from 'utils/fetchAndStoreContent';
+import * as SplashScreen from 'expo-splash-screen';
 
 const App = () => {
   const [showIntro, setShowIntro] = useState(true);
+  const [isReady, setIsReady] = useState(false);
 
   useEffect(() => {
-    fetchAndStoreContent();
+    const fetchData = async () => {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+      } catch (e) {
+        console.warn(e);
+      }
+
+      prepareData();
+    };
+    fetchData();
   }, []);
 
+  async function prepareData() {
+    try {
+      await fetchAndStoreContent();
+    } catch (e) {
+      console.warn(e);
+    } finally {
+      setIsReady(true);
+      await SplashScreen.hideAsync();
+    }
+  }
+
+  if (!isReady) {
+    return null;
+  }
   return showIntro ? (
     <IntroSlider onDone={() => setShowIntro(false)} />
   ) : (
